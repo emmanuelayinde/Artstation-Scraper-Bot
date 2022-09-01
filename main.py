7a1324a780cf6a67946866a63fdfe695379b4802
engr-solution <dah.solu.tion7@gmail.com>
2022-09-01 01:17:50 +0100

    add chromedriver autodownloader lib

--- main.py
+++ main.py
@@ -5,21 +5,27 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
+from keep_alive import alive
 from utils.scraper import scrape_artstation_user
 from utils.users import get_all_users
+import chromedriver_autoinstaller
 
-# Replace with your chromedriver path
-PATH = "C:\Program Files (x86)\chromedriver.exe"
-# CONFIG FOR DEVELOPMENT ENV
+chromedriver_autoinstaller.install()
 
+# CONFIG FOR PRODUCTION ENV
+chrome_options = webdriver.ChromeOptions()
+chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--no-sandbox")
+
+alive()
 users = get_all_users()
-
 i = 0
 
-
+alive()
 while True:
-    driver = webdriver.Chrome(PATH)
+    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)          
     
     user_url = users[i]
     print(user_url)
