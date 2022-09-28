237a37a3c81b6215a62265878427285f238a41cc
engr-solution <dah.solu.tion7@gmail.com>
2022-09-28 07:27:33 +0100

    add selenium options to run bot in vps

--- main.py
+++ main.py
@@ -2,30 +2,35 @@
 import os
 import time
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from keep_alive import alive
+# from keep_alive import alive
 from utils.scraper import scrape_artstation_user
 from utils.users import get_all_users
 import chromedriver_autoinstaller
 
-chromedriver_autoinstaller.install()
 
 # CONFIG FOR PRODUCTION ENV
 chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument("--no-sandbox")
 chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-dev-shm-usage")
-chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-gpu")
+chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
+chrome_options.add_experimental_option('useAutomationExtension', False)
 
 # alive()
 users = get_all_users()
 i = 0
 
-alive()
+# alive()
+
 while True:
-    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)          
+    #driver = webdriver.Chrome(os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
+    driver = webdriver.Chrome(service=Service(chromedriver_autoinstaller.install()), options=chrome_options)         
     
     user_url = users[i]
     print(user_url)
@@ -40,5 +45,5 @@ while True:
     if i >= len(users):
         i = 0
 
-    # Sleep for a minute
-    time.sleep(10)
+    # Sleep for 30secs, then continua
+    time.sleep(30)
